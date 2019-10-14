9c60d4f40fcff066230c979ffe59552aecbe2ccd
Gabriel <gabriel.developer@outlook.com>
2019-10-13 22:41:40 -0300

    adicionando props no projeto react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
+import Tabela from './Tabela';
 
 function App() {
+  const autores = [
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Marcos',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    }
+  ];
+
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <Tabela autores = { autores }/>
     </div>
   );
 }
